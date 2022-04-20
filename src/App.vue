9868ee539ef9756d92dad718db145cf4d6e856ae
Tyler Haddix <tyler.hadx@gmail.com>
2022-04-20 12:49:18 -0600

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -28,11 +28,11 @@
       <v-spacer></v-spacer>
 
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        href="/Blog"
         target="_blank"
         text
       >
-        <span class="mr-2">Latest Release</span>
+        <span class="mr-2">Updates</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
